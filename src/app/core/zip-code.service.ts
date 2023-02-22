d57efb59cec5402a4248e2c068a7c06cd4743b02
Paul Thorsen <paul_thorsen@byu.edu>
2023-02-22 15:11:58 -0700

    refactor dashboard + refactor zipCodeService

---
+++ src/app/core/zip-code.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { config } from './config';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ZipCodeService {
+    private zipCodesSubject = new BehaviorSubject<number[]>([]);
+
+    constructor() {
+        this.getZipCodes();
+    }
+
+    public getZipCodesSubjectAsObservable = () => this.zipCodesSubject.asObservable();
+
+    /**
+     * Adds a zip code to the current array of codes, then calls `setZipCodes()`
+     *
+     * @param zipCode zip code to add
+     */
+    public addZipCode = (zipCode: number): void => {
+        let updatedZipCodes = [...this.zipCodesSubject.value];
+        updatedZipCodes.push(zipCode);
+        this.setZipCodes(updatedZipCodes);
+    };
+
+    /**
+     * Removes a zip code from the current array of codes, then calls `setZipCodes()`
+     *
+     * @param zipCode zip code to add
+     */
+    public removeZipCode = (zipCode: number): void => {
+        let updatedZipCodes = this.zipCodesSubject.value.filter((zip) => zip !== zipCode);
+        this.setZipCodes(updatedZipCodes);
+    };
+
+    /**
+     * Get zip codes from `localStorage`, then emits them via `zipCodesSubject`
+     * @returns zip code array from `localStorage`
+     */
+    private getZipCodes = (): void => {
+        const zipCodes = JSON.parse(
+            localStorage.getItem(config.ZIP_CODES_LOCAL_STORAGE_KEY) ?? '[]'
+        );
+        this.zipCodesSubject.next(zipCodes);
+    };
+
+    /**
+     * Adds zip codes to `localStorage`, the calls `getZipCodes()`
+     * @param zipCodes zip code array to save to `localStorage`
+     */
+    private setZipCodes = (zipCodes: number[]): void => {
+        localStorage.setItem(config.ZIP_CODES_LOCAL_STORAGE_KEY, JSON.stringify(zipCodes));
+        this.getZipCodes();
+    };
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,78 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { combineLatest, Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { forkJoin, iif, Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { WeatherConditionsFromZip } from '../core/models/weather-conditions';
 import { WeatherService } from '../core/weather.service';
-import { ZipCodeManagerService } from '../core/zip-code-manager.service';
+import { ZipCodeService } from '../core/zip-code.service';
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
-    zipCodeInput: number | null = null;
-    zipCodeWeatherConditions$: Observable<WeatherConditionsFromZip[]> = of([]);
-
-    set zipCodeWeatherConditionsObservable(obs: Observable<number[]>) {
-        this.zipCodeWeatherConditions$ = obs.pipe(
-            // Transform observable to Observable<WeatherConditionsFromZip[]>
-            switchMap((zipCodes) => {
-                // combineLatest doesn't emit if there are no inner observables that have emitted (no zipcodes, for example)
-                if (!zipCodes.length) return of([]);
-                // Need to get an observable for each zip code
-                const arr = zipCodes.map((zip) =>
-                    this.weather.getWeatherConditionsByZip(zip).pipe(
-                        // Transform each observable to typeof WeatherConditionFromZip
-                        switchMap((weather) => {
-                            return of({
-                                zipCode: zip,
-                                conditions: weather,
-                            } as WeatherConditionsFromZip);
-                        })
-                    )
-                );
-                // Combine array of observables to observable of array of values
-                return combineLatest(arr);
-            })
-        );
-    }
-
-    showZipCodeHint = false;
-    // Will be hidden if null
-    errorText = '';
+export class DashboardComponent {
     isLoading = false;
+    errorText = '';
+    zipCodeInput = '';
 
-    constructor(private weather: WeatherService, private zipCodeManager: ZipCodeManagerService) {}
+    zipCodeWeatherConditions$: Observable<WeatherConditionsFromZip[]> = this.zipCodeManager
+        .getZipCodesSubjectAsObservable()
+        .pipe(
+            switchMap((zipCodes) =>
+                !zipCodes.length
+                    ? of([])
+                    : forkJoin(
+                          zipCodes.map((zip) =>
+                              this.weather.getWeatherConditionsByZip(zip).pipe(
+                                  // Transform each observable to typeof WeatherConditionFromZip
+                                  map((weather): WeatherConditionsFromZip => {
+                                      return {
+                                          zipCode: zip,
+                                          conditions: weather,
+                                      };
+                                  })
+                              )
+                          )
+                      )
+            )
+        );
 
-    ngOnInit() {
-        // Reactive programming - only subscription is in the template
-        this.zipCodeWeatherConditionsObservable = this.zipCodeManager.getZipCodes();
-    }
+    constructor(private weather: WeatherService, private zipCodeManager: ZipCodeService) {}
 
     public addLocation = (zipCode: number): void => {
         this.errorText = '';
         if (zipCode <= 9999 || zipCode > 99999) {
-            this.showZipCodeHint = true;
+            this.errorText = 'Please enter a 5 digit zip code';
             return;
         }
-        this.showZipCodeHint = false;
-        this.weather.getWeatherConditionsByZip(zipCode).subscribe((weather) => {
-            if (weather) {
-                // Only save zipcode if there was not an error.
-                // Subscribe here because template won't be using this. The Subject emitted from the service will be subscribed to from the template
-                this.zipCodeWeatherConditionsObservable = this.zipCodeManager.addZipCode(zipCode);
-            } else {
+        this.isLoading = true;
+        this.weather.getWeatherConditionsByZip(zipCode).subscribe({
+            next: () => {
+                this.zipCodeManager.addZipCode(zipCode);
+                this.isLoading = false;
+                this.zipCodeInput = '';
+            },
+            error: () => {
                 this.errorText =
                     'Could not find data for zipcode ' + zipCode + '. Please try another zipcode.';
-            }
-            this.isLoading = false;
+                this.isLoading = false;
+            },
         });
-        this.zipCodeInput = null;
     };
 
     public removeLocation = (zipCode: number): void => {
-        // Subscribe here because template won't be using this. The Subject emitted from the service will be subscribed to from the template
-        this.zipCodeWeatherConditionsObservable = this.zipCodeManager.removeZipCode(zipCode);
+        this.zipCodeManager.removeZipCode(zipCode);
     };
 }
